b4e38817db84085d67ee3220a71df902740c3b72
Ribbreaker <13stigul@gmail.com>
2020-03-26 17:59:07 +0100

    Added some extra firepower to the tilesets

---
+++ Assets/PlayerFrontColliderCheck.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFrontColliderCheck : MonoBehaviour
+{
+    public bool WalkingIntoWall;
+    private void OnCollisionStay2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Obstacle") {
+            Debug.Log("collider moved into a wall");
+            WalkingIntoWall = true;
+        }
+        else WalkingIntoWall = false;
+    }
+}
