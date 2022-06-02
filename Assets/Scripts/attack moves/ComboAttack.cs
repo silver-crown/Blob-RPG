bfcb35ba02acf9255de8e0b5e7d1863253e6bc94
laptop thomas <13stigul@gmail.com>
2022-06-03 05:35:53 +0900

    hit detection seems to be good to go

--- Assets/Scripts/attack moves/ComboAttack.cs
+++ Assets/Scripts/attack moves/ComboAttack.cs
@@ -12,10 +12,21 @@ public class ComboAttack : MonoBehaviour
     public string animationName;
     [SerializeField] private bool isUnlocked;
     [SerializeField] protected bool attacking;
-    [SerializeField] public CircleCollider2D[] hitboxes; 
+    [SerializeField] public Collider2D[] hitboxes; 
 
     public virtual void Attack(){
         //activate hitboxes and do funky stuff
+
+        Debug.Log("entered Attack function");
+        attacking = true;
+
+        foreach(Collider2D h in hitboxes){
+            Collider2D[] cols = Physics2D.OverlapCircleAll(h.transform.position, h.bounds.extents.x);
+            foreach(Collider2D c in cols){
+                if (c.gameObject.tag == "Enemy")
+                    Debug.Log(c.name);
+            }
+        } 
     }
 
     void LevelUp(){
